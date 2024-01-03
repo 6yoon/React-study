e337667f29c5dc8f56962375573d4dbf0a81ad03
nayun <nayunk10@gmail.com>
2024-01-04 01:29:25 +0900

    react part 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+let post1 = '망원동 맛집 지도';
+//{}(데이터바인딩)으로 변수를 담을 수 있다
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="makingBlog"> {/* JSX는 className 사용 */}
+        <h3>블로그를 만들어 보아요</h3>
+      </div>
+      <h4 style ={{color : 'green', fontSize : '20px'}}>{post1}</h4>
     </div>
   );
 }
